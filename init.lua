b93d642e26af7dda3a6656ff0ddc6e2e5bbb8552
Spencer <spencer3035@gmail.com>
2025-07-30 21:22:46 -0600

    Much more robust config

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ vim.opt.relativenumber = true;
 vim.pack.add({
     -- BEGIN EXPERIMENTAL PLUGINS
     { src = 'https://github.com/mluders/comfy-line-numbers.nvim' },
+    { src = 'https://github.com/akinsho/toggleterm.nvim' },
     -- END EXPERIMENTAL PLUGINS
     -- For the Pick function
     { src = 'https://github.com/echasnovski/mini.pick' },
@@ -29,6 +30,8 @@ vim.pack.add({
     -- For automatic LSP configuration
     { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
+
+
 
 -- Put help text to right side instead of top
 vim.api.nvim_create_autocmd(
@@ -41,34 +44,120 @@ vim.api.nvim_create_autocmd(
     { pattern = { "*.rs", "*.lua" }, callback = function() vim.lsp.buf.format({ async = false }) end }
 )
 
-function CloseHelpOrTmpWindow()
-    local windows = vim.api.nvim_list_wins();
-    for _, win in ipairs(windows) do
-        local buf = vim.api.nvim_win_get_buf(win)
-        local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
-        if filetype == "help" then
-            vim.api.nvim_win_close(win, false)
+-- BINDINGS
+vim.keymap.set('n', '<leader>so', ':update<CR>:so ' .. vim.fn.expand('~') .. '/.config/nvim/init.lua<CR>')
+vim.keymap.set('n', '<leader>q', ':q')
+vim.keymap.set('n', '<leader>sf', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>sg', ':Pick grep<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.keymap.set('i', '\'', '\'\'<left>');
+vim.keymap.set('i', '"', '""<left>');
+vim.keymap.set('i', '(', '()<left>');
+vim.keymap.set('i', '{', '{}<left>');
+vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O');
+
+vim.keymap.set('n', '<leader>t', ':lua TermTest()<CR>')
+vim.keymap.set('n', '<leader>f', ':lua TermRun()<CR>')
+vim.keymap.set('n', '<leader>d', ':ToggleTerm<CR>')
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+local function debug_function()
+    print("You found my debug function! Nothing do see here")
+end
+
+local function runNewTerminalCommand(cmd)
+    vim.cmd("TermExec direction=vertical cmd=clear")
+    vim.cmd("TermExec direction=vertical cmd=\"" .. cmd .. "\"")
+end
+
+--- Get the command to run for an arbitrary file open in the current buffer
+--- @param isTest boolean If should return test command or run command
+--- @return string? Command to run or nil if couldn't be determined
+local function getProjectCommand(isTest)
+    local ft = vim.api.nvim_get_option_value("filetype", {});
+    -- Full file name
+    local file_name = vim.fn.expand('%:p');
+    local base_name = vim.fs.basename(file_name);
+    if base_name == "Cargo.toml" then
+        if isTest then
+            return "cargo test"
+        else
+            return "cargo run"
         end
+    end
+    if base_name == "Makefile" then
+        if isTest then
+            return "make test"
+        else
+            return "make run"
+        end
+    end
+    if ft == "rust" then
+        if isTest then
+            return "cargo test"
+        else
+            return "cargo run"
+        end
+    end
+
+    -- Could be nil
+    local git_dir = vim.fs.root(0, '.git')
+    -- Traverse files around the git dir to get hints on what to run
+    if git_dir then
+        local base_dir_name = vim.fs.basename(git_dir)
+        if base_dir_name == "nvim" then
+            debug_function()
+            return nil
+        end
+        for entry in vim.fs.dir(git_dir) do
+            if entry == "Cargo.toml" then
+                if isTest then
+                    return "cargo test"
+                else
+                    return "cargo run"
+                end
+            end
+            if entry == "Makefile" then
+                if isTest then
+                    return "make test"
+                else
+                    return "make run"
+                end
+            end
+        end
+    end
+    return nil
+end
+
+function TermRun()
+    local cmd = getProjectCommand(false);
+    if cmd ~= nil then
+        runNewTerminalCommand(cmd)
     end
 end
 
-function RunCommandInTerminal()
-
+function TermTest()
+    local cmd = getProjectCommand(true);
+    if cmd ~= nil then
+        runNewTerminalCommand(cmd)
+    end
 end
 
--- BINDINGS
-
--- Shortcuts for common stuff
-vim.keymap.set('n', '<leader>so', ':update<CR>:so ' .. vim.fn.expand('~') .. '/.config/nvim/init.lua<CR>')
-vim.keymap.set('n', '<leader>q', ':q')
-vim.keymap.set('n', '<leader>d', ':lua CloseHelpOrTmpWindow()<CR>')
-vim.keymap.set('n', '<leader>sf', ':Pick files')
-vim.keymap.set('n', '<leader>sg', ':Pick grep')
-
--- Language server specific bindings
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-
-
+require("toggleterm").setup({
+    size = function(term)
+        if term.direction == "horizontal" then
+            return 15
+        elseif term.direction == "vertical" then
+            -- half of avaliable screen
+            return vim.o.columns * 0.5
+        end
+    end,
+    close_on_exit = true,
+    auto_scroll = true,
+    shade_terminals = true,
+})
 
 require("mason").setup()
 require("mini.pick").setup()
