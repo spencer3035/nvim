2dbc0d03891a74189c1534967b78110782eac9c7
Spencer <spencer3035@gmail.com>
2025-08-15 17:21:29 -0600

    Added TODO section

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+-- My neovim configs
+
+-- TODO:
+-- - Force quit programs running in toggleterm
+-- - <C-j> and <C-k> bindings
+-- - Lua snippet configuration / autocomplete
+
 require('plugins');
 require('settings');
 require('bindings');
