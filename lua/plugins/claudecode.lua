99568ce7f0ffa70443f8d99055a9dcad9107e84e
Spencer <spencer3035@gmail.com>
2026-03-23 11:12:51 -0600

    Updated claudecode bindings

--- lua/plugins/claudecode.lua
+++ lua/plugins/claudecode.lua
@@ -10,11 +10,11 @@ require("claudecode").setup({
     git_repo_cwd = true,
 })
 vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<cr>")
-vim.keymap.set("n", "<leader>af", "<cmd>ClaudeCodeFocus<cr>")
+vim.keymap.set("n", "<leader>al", "<cmd>ClaudeCodeFocus<cr>")
 vim.keymap.set("n", "<leader>ar", "<cmd>ClaudeCode --resume<cr>")
 vim.keymap.set("n", "<leader>aC", "<cmd>ClaudeCode --continue<cr>")
 vim.keymap.set("n", "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>")
-vim.keymap.set("n", "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>")
+vim.keymap.set("n", "<leader>af", "<cmd>ClaudeCodeAdd %<cr>")
 vim.keymap.set("v", "<leader>as", "<cmd>ClaudeCodeSend<cr>")
 -- Diff management
 vim.keymap.set("n", "<leader>ay", "<cmd>ClaudeCodeDiffAccept<cr>")
