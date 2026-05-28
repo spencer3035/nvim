07c5fd8a8ad126f20a488fd71ea5ebe7013eac8d
Spencer <spencer3035@gmail.com>
2026-05-28 12:19:50 -0600

    added some common snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,4 +1,68 @@
 ---@diagnostic disable: undefined-global
 
 -- Snippets for lua
-return {}
+return {
+    -- For parnethesis with cursor on inside
+    s(
+        {
+            trig = ";p",
+            wordTrig = false,
+            snippetType = "autosnippet"
+        },
+        fmta(
+            [[
+                (<stop>)
+            ]],
+            {
+                stop = i(0),
+            }
+        )
+    ),
+    -- For single quotes with cursor on inside
+    s(
+        {
+            trig = ";s",
+            wordTrig = true,
+            snippetType = "autosnippet"
+        },
+        fmta(
+            [[
+                '<stop>'
+            ]],
+            {
+                stop = i(0),
+            }
+        )
+    ),
+    -- For double quotes with cursor on inside
+    s(
+        {
+            trig = ";d",
+            wordTrig = true,
+            snippetType = "autosnippet"
+        },
+        fmta(
+            [[
+                "<stop>"
+            ]],
+            {
+                stop = i(0),
+            }
+        )
+    ),
+    s(
+        {
+            trig = ";b",
+            wordTrig = true,
+            snippetType = "autosnippet"
+        },
+        fmta(
+            [[
+                [<stop>]
+            ]],
+            {
+                stop = i(0),
+            }
+        )
+    ),
+}
